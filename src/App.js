4e7886815671eeae8ecb2d84d22c6ad76b13cc37
saumitra vishal <saumitravishal3@gmail.com>
2019-12-24 18:10:21 +0530

    basic routing set-up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import HomePage from "./pages/homepages/homepage.component";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import "./pages/homepages/homepage.styles.scss";
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
